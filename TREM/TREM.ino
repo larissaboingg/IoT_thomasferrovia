c91f996878b6669be922243d3bcc567d5bea9451
Laura <laura_kasten@estudante.sesisenai.org.br>
2025-10-20 12:17:37 -0300

    Conexão com broker

--- TREM/TREM.ino
+++ TREM/TREM.ino
@@ -1,6 +1,17 @@
 #include <WiFi.h>
+#include <PubSubClient.h>
+
+WiFiClient client;        //cria cliente wifi
+PubSubClient mqtt(client);  //fala que mqtt usa o cliente wifi
+
 const String SSID = "FIESC_IOT_EDU";
 const String PASS = "8120gv08";
+
+const int PORT           =1883;
+const String URL         ="test.mosquitto.org";
+const String TOPIC       ="DSM2";
+const String broker_user ="";
+const String broker_pass = "";
 
 void setup() {
   Serial.begin(115200);
@@ -11,6 +22,16 @@ void setup() {
     delay(200);
   }
   Serial.println("\nConectado!");
+  Serial.println("Conectando ao broker...");
+  mqtt.setServer(URL.c_str(),PORT);
+  while(!mqtt.connected()){
+    String ID = "TREM-";
+    ID += String(random(0xffff),HEX);
+    mqtt.connect(ID.c_str(),broker_user.c_str(),broker_pass.c_str());
+    delay(200);
+    Serial.print(".");
+  }
+  Serial.println("\nConectado ao broker com sucesso!");
 }
 
 void loop() {
