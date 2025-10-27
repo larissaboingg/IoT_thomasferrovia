17612cd1f3f77854cee0308a56c60027ba0ecc58
Eduarda <eduarda_tomaz@estudante.sesisenai.org.br>
2025-10-27 12:49:23 -0300

    envio e recebimento de msgs

--- S3/S3.ino
+++ S3/S3.ino
@@ -9,11 +9,17 @@ const String PASS = "8120gv08";
 
 const int PORT           =1883;
 const String URL         ="test.mosquitto.org";
-const String TOPIC       ="DSM2";
+
 const String broker_user ="";
 const String broker_pass = "";
 
+const String MyTopic    ="Et_topico"; //define da onde vou receber as msgs
+const String OtherTopic ="Lk_topico"; //define da onde vou enviar as msgs
+
+const int ledPin = 2;
+
 void setup() {
+  pinMode(ledPin, OUTPUT);
   Serial.begin(115200);
   Serial.println("Conectando ao Wifi");
   WiFi.begin(SSID,PASS);
@@ -31,14 +37,30 @@ void setup() {
     delay(200);
     Serial.print(".");
   }
+  mqtt.subscribe(MyTopic.c_str());
+  mqtt.setCallback(callback);
   Serial.println("\nConectado ao broker com sucesso!");
 }
 
 void loop() {
-  String mensagem ="Eduarda:  ";
-  mensagem += "oi";
-
-  mqtt.publish(TOPIC.c_str(), mensagem.c_str());
+  String mensagem ="";
+  if(Serial.available()>0){
+  mensagem += Serial.readStringUntil('\n'); // le mensagens que o usuário digitou 
+  mqtt.publish(OtherTopic.c_str(), mensagem.c_str()); // envia a msg para o topico do colega
+  }
   mqtt.loop();
-  delay(2000);
+}
+
+void callback(char* topic, byte* payload, unsigned int length){
+  String mensagem = "";
+  for(int i = 0; i < length; i++){
+    mensagem += (char)payload[i];
+  }
+  Serial.print("Recebido: ");
+  Serial.println(mensagem);
+  if(mensagem == "Acender"){
+    digitalWrite(ledPin, HIGH);
+  } else if (mensagem == "Apagar"){
+    digitalWrite(ledPin, LOW);
+  }
 }
