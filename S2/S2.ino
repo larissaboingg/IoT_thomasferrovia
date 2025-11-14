4a791fcb615edb38841cfb89f2259fac16698609
larissaboingg <larissa_boing@estudante.sesisenai.org.br>
2025-11-14 12:31:52 -0300

    S2 concluido

--- S2/S2.ino
+++ S2/S2.ino
@@ -1,72 +1,128 @@
-#include <WiFi.h>
+#include <WiFiClientSecure.h>
 #include <PubSubClient.h>
 
-WiFiClient client;        //cria cliente wifi
+WiFiClientSecure client;    //cria cliente wifi
 PubSubClient mqtt(client);  //fala que mqtt usa o cliente wifi
 
 const String SSID = "FIESC_IOT_EDU";
 const String PASS = "8120gv08";
 
-const int PORT           =1883;
-const String URL         ="test.mosquitto.org";
+const int PORT = 8883;
+const String URL = "c2388c77116243a0a57bd57d29c9b376.s1.eu.hivemq.cloud";
 
-const String broker_user ="";
-const String broker_pass = "";
+const String broker_user = "Sensor2";
+const String broker_pass = "Larissa123";
 
-const String MyTopic = "TopicLarissa"; //define de onde vou receber as msgs
-const String OtherTopic = "TopicGabriela"; //definepara onde vou enviar as msgs
-const int ledAzul = 2;
+//recebe
+const char* iluminacao = "iluminacao";
+
+//envia
+const char* presenca_2 = "presenca2";
+const char* presenca_3 = "presenca3";
+
+const int ledPin = 2;
+const byte TRIGGER_1 = 5;
+const byte ECHO_1 = 12;
+
+const byte TRIGGER_2 = 6;
+const byte ECHO_2 = 13;
 
 void setup() {
   Serial.begin(115200);
+
+  pinMode(ledPin, OUTPUT);
+  pinMode(TRIGGER_1, OUTPUT);
+  pinMode(ECHO_1, INPUT);
+  pinMode(TRIGGER_2, OUTPUT);
+  pinMode(ECHO_2, INPUT);
+
   Serial.println("Conectando ao Wifi");
-  WiFi.begin(SSID,PASS);
-  while(WiFi.status() != WL_CONNECTED){
+  WiFi.begin(SSID, PASS);
+  while (WiFi.status() != WL_CONNECTED) {
     Serial.print(".");
     delay(200);
   }
   Serial.println("\nConectado!");
+  client.setInsecure();
   Serial.println("Conectando ao broker...");
-  mqtt.setServer(URL.c_str(),PORT);
-  while(!mqtt.connected()){
+  mqtt.setServer(URL.c_str(), PORT);
+  while (!mqtt.connected()) {
     String ID = "S2-";
-    ID += String(random(0xffff),HEX);
-    mqtt.connect(ID.c_str(),broker_user.c_str(),broker_pass.c_str());
+    ID += String(random(0xffff), HEX);
+    mqtt.connect(ID.c_str(), broker_user.c_str(), broker_pass.c_str());
     delay(200);
     Serial.print(".");
   }
-  mqtt.subscribe(MyTopic.c_str());
+
+  mqtt.subscribe(presenca_2);
+  mqtt.subscribe(presenca_3);
+  mqtt.subscribe(iluminacao);
+
   mqtt.setCallback(callback);
   Serial.println("\nConectado ao broker com sucesso!");
-  pinMode(ledAzul, OUTPUT);
+}
+
+long lerDistancia1() {
+  digitalWrite(TRIGGER_1, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIGGER_1, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIGGER_1, LOW);
+
+  long duracao1 = pulseIn(ECHO_1, HIGH);
+  long distancia1 = duracao1 * 349.24 / 2 / 10000;
+
+  return distancia1;
+}
+
+long lerDistancia2() {
+  digitalWrite(TRIGGER_2, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIGGER_2, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIGGER_2, LOW);
+
+  long duracao2 = pulseIn(ECHO_2, HIGH);
+  long distancia2 = duracao2 * 349.24 / 2 / 10000;
+
+  return distancia2;
 }
 
 void loop() {
-  String mensagem = "";
-  if(Serial.available()>0){
-    mensagem += Serial.readStringUntil('\n'); //le mensagens que o usuário digitou
-    mqtt.publish(OtherTopic.c_str(), mensagem.c_str()); // envia a msg para p topico do colega
+  long distancia1 = lerDistancia1();
+
+  Serial.print("Distância: ");
+  Serial.print(distancia1);
+  Serial.println(" cm");
+
+  if (distancia1 < 10) {
+    mqtt.publish(presenca_2, "detectado");
+  }
+  long distancia2 = lerDistancia2();
+
+  Serial.print("Distância: ");
+  Serial.print(distancia2);
+  Serial.println(" cm");
+
+  if (distancia2 < 10) {
+    mqtt.publish(presenca_3, "detectado");
   }
 
   mqtt.loop();
-  delay(2000);
+  delay(10);
 }
 
-void callback(char* topic, byte* payload, unsigned int length){
+void callback(char* topic, byte* payload, unsigned int length) {
   String mensagem = "";
-  for(int i = 0; i < length; i++){
+  for (int i = 0; i < length; i++) {
     mensagem += (char)payload[i];
   }
   Serial.print("Recebido: ");
   Serial.println(mensagem);
-  if(mensagem == ("acender")){
-    digitalWrite(ledAzul, HIGH);
-  }else if(mensagem == ("apagar")){
-    digitalWrite(ledAzul, LOW);
+  else if (topic == iluminacao && mensagem == "acender") {
+    digitalWrite(ledPin, HIGH);
   }
-  else{
-    Serial.println("Inválido");
+  else if (topic == iluminacao && mensagem == "apagar") {
+    digitalWrite(ledPin, LOW);
   }
 }
-
-
